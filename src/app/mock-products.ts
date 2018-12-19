140cca95c876e0d1d1badf08fd86c8d0e7d2f376
aravin <aravindannair24@gmail.com>
2018-12-18 20:43:40 -0500

    Add mock products list
    - this will be later a GET call on /Products

---
+++ src/app/mock-products.ts
@@ -0,0 +1,41 @@
+import { Product } from './product';
+
+export const  MOCKPRODUCTS: Product [] = [
+    {
+        productid: 1, 
+        product: 'DS 400 Camera Camera', 
+        productPrice: 1000, 
+        productQty: 1
+    },
+    {
+        productid: 2, 
+        product: 'Sony Cybershot Camera', 
+        productPrice: 2000, 
+        productQty: 1
+    },
+    {
+        productid: 3, 
+        product: 'Nikon Coolpix B500 Camera', 
+        productPrice: 3000, 
+        productQty: 1
+    },
+    {
+        productid: 4, 
+        product: 'Canon EOS Rebel T6 Camera', 
+        productPrice: 4000, 
+        productQty: 1
+    },
+    {
+        productid: 5, 
+        product: 'Fujifilm X-T 100 Mirrorless', 
+        productPrice: 5000, 
+        productQty: 1
+    },
+    {
+        productid: 6, 
+        product: 'Nikon D850 FX-Series Digital Body', 
+        productPrice: 6000, 
+        productQty: 1
+    }
+
+];
