e909b86f72565571206cf02862a870cad3d5b51e
aravindanb <aravindannair24@gmail.com>
2019-04-08 19:08:29 -0400

    Replace MOCK service with Order Service
     - Modify the product service to use the actual Order Service endpoint

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,22 +6,26 @@ import { Observable, of } from 'rxjs';
 
 import { Product } from './product';
 import { MOCKPRODUCTS } from './mock-products';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private productsUrl = 'http://localhost:3000/orders/';
 
   // method to return mock products
   // todo: call an endpoint to return this array later
+
   // getProducts(): Observable<Product[]> {
   //   return of(MOCKPRODUCTS);
   // }
 
-  getProducts() : Observable<Product[]>{
-    return of(MOCKPRODUCTS);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
   }
 
   // method to return mock product by id
