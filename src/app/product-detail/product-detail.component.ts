4bc8f021a848106e0f9200abca6825e3a91c6df2
aravindanb <aravindannair24@gmail.com>
2019-05-17 19:05:59 -0400

    remove scss

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -7,7 +7,7 @@ import { ProductService} from '../product.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
 
