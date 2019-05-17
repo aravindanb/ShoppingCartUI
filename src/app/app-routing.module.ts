60908b33492a0eb84839d4d10fb3a3c299e2c7b2
aravindanb <aravindannair24@gmail.com>
2019-05-17 17:30:44 -0400

    Add login component and related packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   { path: 'productdetails/:productId', component: ProductDetailComponent },
   { path: 'productcatalogue', component: ProductListComponent },
   { path: 'shoppingcart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/productcatalogue', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductDetailComponent,
-    CartComponent
+    CartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
