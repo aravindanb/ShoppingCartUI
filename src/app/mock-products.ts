e664e88861cbcb5d619a22fb0fbb6e6debb31cf1
aravindanb <aravindannair24@gmail.com>
2019-05-17 15:21:39 -0400

    add grid layout

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -2,42 +2,42 @@ import { Product } from './product';
 
 export const  MOCKPRODUCTS: Product [] = [
     {
-        productid: '1',
+        productId: '1',
         _id: '1',
         product: 'DS 400 Camera Camera',
         productPrice: 1000,
         productQty: 1
     },
     {
-        productid: '2',
+        productId: '2',
         _id: '2',
         product: 'Sony Cybershot Camera',
         productPrice: 2000,
         productQty: 1
     },
     {
-        productid: '3',
+        productId: '3',
         _id: '3',
         product: 'Nikon Coolpix B500 Camera',
         productPrice: 3000,
         productQty: 1
     },
     {
-        productid: '4',
+        productId: '4',
         _id: '4',
         product: 'Canon EOS Rebel T6 Camera',
         productPrice: 4000,
         productQty: 1
     },
     {
-        productid: '5',
+        productId: '5',
         _id: '5',
         product: 'Fujifilm X-T 100 Mirrorless',
         productPrice: 5000,
         productQty: 1
     },
     {
-        productid: '6',
+        productId: '6',
         _id: '6',
         product: 'Nikon D850 FX-Series Digital Body',
         productPrice: 6000,
